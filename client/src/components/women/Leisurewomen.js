f859d2fd31665ee4fa763ead7435a69e3e0b8ddb
GreatEki <ekeneidiagbor@gmail.com>
2020-04-18 19:29:47 -0900

    Product fetching from json server for Women Category

--- client/src/components/women/Leisurewomen.js
+++ client/src/components/women/Leisurewomen.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import Navbar from '../templates/Navbar';
 import Navwomen from '../templates/Navwomen';
 import { Link } from 'react-router-dom';
+import { ProductsContext } from '../../contexts/ProductsContext';
 const Leisurewomen = () => {
+	const { getLeisureWomenProducts, leisureWomen } = useContext(ProductsContext);
+
+	useEffect(() => {
+		getLeisureWomenProducts();
+
+		//eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
 	return (
 		<>
 			<div className='wrapper container-fluid m-0 p-0'>
@@ -30,33 +38,45 @@ const Leisurewomen = () => {
 
 				<main className='container-fluid content-wrapper justify-content-center px-lg-5 px-md-2'>
 					<div className='row mt-5 px-lg-5 px-md-2 pt-5'>
-						<div className='col-lg-2 col-md-6 col-sm-12 shop-gallery'>
-							<div className='card  item-card mb-3'>
-								<div className='row no-gutters'>
-									<div className='col-md-4 col-lg-12'>
-										<img
-											src='/products/stock-jeans-ladies-3.jpg'
-											className='card-img'
-											alt='product-name'
-										/>
-									</div>
-									<div className='col-md-8 col-lg-12'>
-										<div className='card-body'>
-											<p className='card-title'>High Waist Ladies Jeans</p>
-											<h5 className='card-text'>
-												<del>N</del> 8, 000
-											</h5>
-											<p className='card-text'>
-												<small className='text-muted'>Available in Stock</small>
-											</p>
-											<button className='btn btn-block btn-outline-success text-center'>
-												Buy{' '}
-											</button>
+						{/*====================== Products Display Starts Here  ===========================*/}
+
+						{leisureWomen.map((product) => {
+							return (
+								<div
+									className='col-lg-2 col-md-6 col-sm-12 shop-gallery'
+									key={product.id}>
+									<div className='card  item-card mb-3'>
+										<div className='row no-gutters'>
+											<div className='col-md-4 col-lg-12'>
+												<img
+													src={`/products/${product.imagePath}`}
+													className='card-img'
+													alt='product-name'
+												/>
+											</div>
+											<div className='col-md-8 col-lg-12'>
+												<div className='card-body'>
+													<p className='card-title'>{product.title}</p>
+													<h5 className='card-text'>
+														<del>N</del> {product.price}
+													</h5>
+													<p className='card-text'>
+														<small className='text-muted'>
+															Available in Stock
+														</small>
+													</p>
+													<button className='btn btn-block btn-outline-success text-center'>
+														Buy{' '}
+													</button>
+												</div>
+											</div>
 										</div>
 									</div>
 								</div>
-							</div>
-						</div>
+							);
+						})}
+
+						{/*====================== End of Products Display  ===========================*/}
 					</div>
 				</main>
 			</div>
--- client/src/contexts/ProductsContext.js
+++ client/src/contexts/ProductsContext.js
@@ -8,6 +8,10 @@ const ProductsContextProvider = (props) => {
 	const [businessMen, setBusinessMen] = useState([]);
 	const [cultureMen, setCultureMen] = useState([]);
 	const [leisureMen, setLeisureMen] = useState([]);
+	const [businessWomen, setBusinessWomen] = useState([]);
+	const [cultureWomen, setCultureWomen] = useState([]);
+	const [everyDayWomen, setEveryDayWomen] = useState([]);
+	const [leisureWomen, setLeisureWomen] = useState([]);
 
 	//DESC: Fetches products for our EveryDayMen Component
 	//URL: /products?category=everyday&gender=men&gender=unisex
@@ -68,6 +72,64 @@ const ProductsContextProvider = (props) => {
 			console.log(err.message);
 		}
 	};
+
+	//DESC: Fetches products for our BusinessWomen Component
+	//URL:  /products?category=business&gender=women&gender=unisex'
+	//REQUEST: GET
+	const getBusinessWomenProducts = async () => {
+		try {
+			const res = await axios.get(
+				'http://localhost:3004/products?category=business&gender=women&gender=unisex'
+			);
+
+			setBusinessWomen(res.data);
+		} catch (err) {
+			console.log(err.message);
+		}
+	};
+	//DESC: Fetches products for our cultureWomen Component
+	//URL:  /products?category=business&gender=women&gender=unisex'
+	//REQUEST: GET
+	const getCultureWomenProducts = async () => {
+		try {
+			const res = await axios.get(
+				'http://localhost:3004/products?category=native&gender=women'
+			);
+
+			setCultureWomen(res.data);
+		} catch (err) {
+			console.log(err.message);
+		}
+	};
+
+	//DESC: Fetches products for our everyDayWomen Component
+	//URL:  /products?category=everyday&gender=women&gender=unisex'
+	//REQUEST: GET
+	const geteveryDayWomenProducts = async () => {
+		try {
+			const res = await axios.get(
+				'http://localhost:3004/products?category=everyday&gender=women&gender=unisex'
+			);
+
+			setEveryDayWomen(res.data);
+		} catch (err) {
+			console.log(err.message);
+		}
+	};
+	//DESC: Fetches products for our everyDayWomen Component
+	//URL:  /products?category=everyday&gender=women&gender=unisex'
+	//REQUEST: GET
+	const getLeisureWomenProducts = async () => {
+		try {
+			const res = await axios.get(
+				'http://localhost:3004/products?category=leisure&gender=women&gender=unisex'
+			);
+
+			setLeisureWomen(res.data);
+		} catch (err) {
+			console.log(err.message);
+		}
+	};
 	return (
 		<ProductsContext.Provider
 			value={{
@@ -79,6 +141,14 @@ const ProductsContextProvider = (props) => {
 				cultureMen,
 				getLeisureMenProducts,
 				leisureMen,
+				getBusinessWomenProducts,
+				businessWomen,
+				getCultureWomenProducts,
+				cultureWomen,
+				geteveryDayWomenProducts,
+				everyDayWomen,
+				getLeisureWomenProducts,
+				leisureWomen,
 			}}>
 			{props.children}
 		</ProductsContext.Provider>
